7d70131d1b8c8d12e27afb2e496bdfab51e20351
Shikhar <shikharr521@gmail.com>
2026-08-03 10:12:32 +0000

    making the server to start doing Non-Blocking I/O

--- server.cpp
+++ server.cpp
@@ -9,18 +9,119 @@
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 #include <stdbool.h>
-
-static void die(const char *msg) {
-    int err = errno;
-    fprintf(stderr, "[%d] %s\n", err, msg);
-    abort();
-}
+#include <vector>
+#include <fcntl.h>
+using namespace std;
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
 
-const size_t MAX_REQUEST_SIZE = 4096;
+static void msg_errno(const char *msg) {
+    fprintf(stderr, "[errno:%d] %s\n", errno, msg);
+}
+
+static void die(const char *msg) {
+    fprintf(stderr, "[%d] %s\n", errno, msg);
+    abort();
+}
+
+const size_t MAX_REQUEST_SIZE = 32<<20; //This line defines a constant MAX_REQUEST_SIZE with a value of 32 shifted left by 20 bits,
+// which is equivalent to 32 * 2^20 = 33,554,432 bytes (32 MB).
+
+struct Conn{ //Object to represent a connection. It contains the following members:
+    int fd=-1; 
+    bool want_read=false;
+    bool want_write=false;
+    bool want_close=false;
+    std::vector<uint8_t> incoming; //buffered incoming data
+    std::vector<uint8_t> outgoing; //buffered outgoing data
+
+};
+
+static void fd_set_nb(int fd){ //This function sets the file descriptor fd to non-blocking mode using the fcntl() system call.
+    errno=0;
+    int flags=fcntl(fd,F_GETFL,0); //This line retrieves the current file
+    if(errno){
+        die("fcntl error");
+        return;
+    }
+    flags |= O_NONBLOCK;
+    errno = 0;
+    (void)fcntl(fd, F_SETFL, flags);
+    if (errno) {
+        die("fcntl error");
+    }
+
+}
+
+static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) { //This function appends data to a buffer 
+    //represented by a std::vector<uint8_t> called buf.
+    buf.insert(buf.end(), data, data + len);
+}
+
+static void buf_consume(std::vector<uint8_t> &buf, size_t n) { //THis function removes the first n bytes from the
+    // buffer represented by a std::vector<uint8_t> called buf.
+    buf.erase(buf.begin(), buf.begin() + n);
+}
+
+static Conn *handle_accept(int fd) { //This function handles the acceptance of a new connection on the listening socket
+    // represented by the file descriptor fd.
+    // accept
+    struct sockaddr_in client_addr = {};
+    socklen_t addrlen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+    if (connfd < 0) {
+        msg_errno("accept() error");
+        return NULL;
+    }
+    uint32_t ip = client_addr.sin_addr.s_addr;
+    fprintf(stderr, "new client from %u.%u.%u.%u:%u\n",
+        ip & 255, (ip >> 8) & 255, (ip >> 16) & 255, ip >> 24,
+        ntohs(client_addr.sin_port)
+    );
+
+    // set the new connection fd to nonblocking mode
+    fd_set_nb(connfd);
+
+    // create a `struct Conn`
+    Conn *conn = new Conn();
+    conn->fd = connfd;
+    conn->want_read = true;
+    return conn;
+}
+
+static bool try_one_request(Conn *conn) {
+    // try to parse the protocol: message header
+    if (conn->incoming.size() < 4) {
+        return false;   // want read
+    }
+    uint32_t len = 0;
+    memcpy(&len, conn->incoming.data(), 4);
+    if (len > MAX_REQUEST_SIZE) {
+        msg("too long");
+        conn->want_close = true;
+        return false;   // want close
+    }
+    // message body
+    if (4 + len > conn->incoming.size()) {
+        return false;   // want read
+    }
+    const uint8_t *request = &conn->incoming[4];
+
+    // got one request, do some application logic
+    printf("client says: len:%d data:%.*s\n",
+        len, len < 100 ? len : 100, request);
+
+    // generate the response (echo)
+    buf_append(conn->outgoing, (const uint8_t *)&len, 4);
+    buf_append(conn->outgoing, request, len);
+
+    // application logic done! remove the request message.
+    buf_consume(conn->incoming, 4 + len);
+    // Q: Why not just empty the buffer? See the explanation of "pipelining".
+    return true;        // success
+}
 
 static int32_t read_all(int fd,char *buf,size_t size){ //This function reads data from a file descriptor (fd) into a buffer (buf) until the specified size (size) is reached. It returns 0 on success and -1 on failure.
     while(size>0){
@@ -49,7 +150,9 @@ static int32_t write_all(int fd,char *buf,size_t size){
     return 0;
 }
 
-static int32_t one_request(int connfd) {
+//This function is the protocol parser and handler for a single request. 
+//It reads the request from the connection represented by connfd, processes it, and sends a reply back to the client.
+static int32_t one_request(int connfd) { 
     // 4 bytes header
     char rbuf[4 + MAX_REQUEST_SIZE];
     errno = 0;
